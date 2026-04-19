a52895ea3513865d9735227c5f300c04f85c9b1f
pprejs20 <pprejs22@gmail.com>
2026-04-19 12:45:59 +0100

    Change Suggestion model to be a record

--- VitrueWebApi/VitrueWebAPI/Models/Suggestion.cs
+++ VitrueWebApi/VitrueWebAPI/Models/Suggestion.cs
@@ -5,17 +5,17 @@
     public enum SuggestionPriority { Low, Medium, High }
     public enum SuggestionSource { Vida, Admin }
 
-    public class Suggestion
+    public record Suggestion
     {
         public required Guid Id { get; init; }
         public required Guid EmployeeId { get; init; }
-        public required SuggestionType Type { get; set; }
-        public required string Description { get; set; }
-        public required SuggestionStatus Status { get; set; }
-        public required SuggestionPriority Priority { get; set; }
+        public required SuggestionType Type { get; init; }
+        public required string Description { get; init; }
+        public required SuggestionStatus Status { get; init; }
+        public required SuggestionPriority Priority { get; init; }
         public required SuggestionSource Source { get; init; }
         public required DateTime DateCreated { get; init; }
-        public required DateTime DateUpdated { get; set; }
-        public string Notes { get; set; } = string.Empty;
+        public required DateTime DateUpdated { get; init; }
+        public string Notes { get; init; } = string.Empty;
     }
 }
